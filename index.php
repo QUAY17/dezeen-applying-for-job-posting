58808afad74cd361007613cd530e5fdc07559879
Jennifer Minnich <jminniecc@gmail.com>
2017-01-12 08:45:10 -0700

    committing <ul> for technology section

--- index.php
+++ index.php
@@ -10,30 +10,35 @@
 			</header>
 		<main>
 			<img src="images/jose.jpg" alt="jose' ferrufino" align="left" width="50%">
+
 			<h2 align="left">Persona: Jos&eacute;</h2>
-				<p><b>Name</b>: Jose' Ferrufino<br></p>
+				<p><b>Name</b>: Jos&eacute; Ferrufino<br></p>
 				<p><b>Age</b>: 33</p>
 				<p><b>Profession</b>: Luxury Product Design Professional</p>
-				<p><b>Technology</b>: MacPro Desktop(@work- design studio) MacBook Pro(@home- newer model and updated IOS, external hard drive), iPhone6s or higher(@mobile- International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work). Absolutely efficient in Windows, but in the design industry, Mac is preferred.</p>
+			<p><b>Technology</b>:
+				<ul>
+				<li>MacPro Desktop(<b>work</b>: design studio)</li>
+				<li>MacBook Pro(<b>home</b>: newer model and updated IOS, external hard drive)</li>
+				<li>iPhone6s or higher(<b>mobile</b>: International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work)</li>
+				<li>Absolutely efficient in Windows, but in the design industry, Mac is preferred.</li>
+			</ul>
 				<p><b>Attitudes and Behaviors</b>:Seasoned young professional with over a decade of experience in luxury product design. Not applying for jobs below his salary or expectation scope. Works internationally; has been employed in New York, Paris, and Lasaunne, Switzerland. Elevated sensibility.</p>
 				<p><b>Frustrations and Needs</b>:Jose' is not looking to be hired in any other market aside from the luxury niche. He is not looking to work for a startup either (except his own, "Les Chardons-Bleus" which is located in Paris- working with his team on his own time). Salary expectations must be met as well as product expectations. Seeking "long-term" with growth potential, new contracts, expansion, etc.</p>
 			<p><b>Main Goal</b>:
-				Landing a full-time, high-end design position in the US, Europe, or Asia which will be "home" for at least 3 years. Currently residing in nyc, relocation is a welcomed option and will be considered given the appropriate remuneration. As previously stated, emphasis on vertical growth potential within the company.</p>
+				Landing a full-time, high-end design position in the US, Europe, or Asia which will be "home" for at least 3 years. Currently residing in Brooklyn, relocation is a welcomed option and will be considered given the appropriate remuneration. As previously stated, emphasis on vertical growth potential within the company.</p>
+
 		<h3>Use Case: How & Why</h3>
-			<p>Jose' will use Dezeen to search for job opportunities, filter through positions meaningful to his career scope, and then apply directly to employers posting openings .</p>
-			<br>
-			<br>
-			<br>
-			<br>
+			<p>Jos&eacute; will use Dezeen to search for job opportunities, filter through positions meaningful to his career scope, and then apply directly to potential employers.</p>
+
 		<h3 align="left">Interaction Flow: Specific Steps</h3>
 				<ol>
-					<li>Jose' visits <a href="https://www.dezeenjobs.com"></a>Dezeen Jobs</li>
+					<li>Jos&eacute; visits <a href="https://www.dezeenjobs.com">Dezeen Jobs</a></li>
 					<li>Dezeen displays list of job opportunities in most recent chronological order</li>
-					<li>Jose' scrolls through jobs and selects most fitting listing (by clicking on job title link)</li>
+					<li>Jos&eacute; scrolls through jobs and selects most fitting listing (by clicking on job title link)</li>
 					<li>Dezeen displays full job description containing email of employer receiving applicants</li>
-					<li>Jose' submits all applicant requirements to said employer email</li>
+					<li>Jos&eacute; submits all applicant requirements to said employer email</li>
 					<li>Employer receives Jose's submission, is "wow'd" by his accomplishments and sets up an interview</li>
-					<li>Jose is pleased and drinks champagne in anticipation of getting a great new job! Lives happily ever after...</li>
+					<li>Jos&eacute; is pleased and drinks champagne in anticipation of getting a great new job! Lives happily ever after...</li>
 				</ol>
 		</main>
 		</body>
