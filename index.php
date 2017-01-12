dd6d94c0cfe3e6b91fbdbd979e3567f2563bf8b6
Jennifer Minnich <jminniecc@gmail.com>
2017-01-11 20:51:00 -0700

    committing pic of jose' with layout corrections

--- index.php
+++ index.php
@@ -8,19 +8,19 @@
 	</head>
 	<body>
 		<header>
-			<h1 align="center">Applying for a job posting</h1>
+			<h1 align="center">Applying for a job posting on Dezeen</h1>
 			</header>
 		<main>
 				<h2 >Persona: Who</h2>
-			<img src="/images/jose.jpg"
+			<img src="images/jose.jpg" alt="jose' ferrufino" align="left" width="50%">
 			<p>
-Name: Jose' Ferrufino
-Age: 33
-Profession: Luxury Product Design Professional
-Technology: MacPro Desktop(@work- design studio) MacBook Pro(@home- newer model and updated IOS, external hard drive), iPhone6s or higher(@mobile- International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work). Absolutely efficient in Windows, but in the design industry, Mac is preferred.
-Attitudes and Behaviors:
-Frustrations and Needs:
-Goals:
+				Name: Jose' Ferrufino<br>
+				Age: 33<br>
+				Profession: Luxury Product Design Professional<br>
+				Technology: MacPro Desktop(@work- design studio) MacBook Pro(@home- newer model and updated IOS, external hard drive), iPhone6s or higher(@mobile- International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work). Absolutely efficient in Windows, but in the design industry, Mac is preferred.<br>
+				Attitudes and Behaviors:<br>
+				Frustrations and Needs:<br>
+				Goals:
 </p>
 				<h3>Use Case: Main Goal</h3>
 				<h3>Interaction Flow: Specific Steps</h3>
