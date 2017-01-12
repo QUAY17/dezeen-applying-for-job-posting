6f116a04b2231d1647945af3e109e62f5e288a6d
Jennifer Minnich <jminniecc@gmail.com>
2017-01-12 09:55:58 -0700

    committing final html

--- index.php
+++ index.php
@@ -6,20 +6,20 @@
 	</head>
 	<body>
 		<header>
-			<h1>APPLYING FOR A JOB ON DEZEEN</h1>
+			<h1 align="center">APPLYING FOR A JOB ON DEZEEN</h1>
 			</header>
 		<main>
 			<img src="images/jose.jpg" alt="jose' ferrufino" align="center" width="45%" height="50%">
 
-			<h2 align="left">Persona: Jos&eacute;</h2>
+			<h2 align="left">Persona:</h2>
 				<p><b>Name</b>: Jos&eacute; Ferrufino<br></p>
 				<p><b>Age</b>: 33</p>
 				<p><b>Profession</b>: Luxury Product Design Professional</p>
 			<p><b>Technology</b>:
 				<ul>
-				<li><b>MacPro Desktop</b> (work: design studio)</li>
-				<li><b>MacBook Pro</b> (home: newer model and updated IOS, external hard drive)</li>
-				<li><b>iPhone6s or higher</b> (mobile: International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work)</li>
+				<li><b>MacPro Desktop</b> (work: design studio, updated IOS X Sierra)</li>
+				<li><b>MacBook Pro</b> (home: newer model and updated IOS X Sierra, external hard drive)</li>
+				<li><b>iPhone6s or higher</b> (mobile: IOS 10.2 with international data plan. Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work)</li>
 				<li>Proficient in Windows, but in the design industry, Mac is preferred.</li>
 			</ul>
 				<p><b>Attitudes and Behaviors</b>:Seasoned young professional with over a decade of experience in luxury product design. Not applying for jobs below his salary or expectation scope. Works internationally; has been employed in New York, Paris, and Lasaunne, Switzerland. Elevated sensibility.</p>
@@ -32,11 +32,11 @@
 
 		<h3>Interaction Flow: Specific Steps</h3>
 				<ol>
-					<li>Jos&eacute; visits <a href="https://www.dezeenjobs.com">Dezeen Jobs</a></li>
+					<li>Jos&eacute; opens Chrome on his MacBook and visits <a href="https://www.dezeenjobs.com">Dezeen Jobs</a></li>
 					<li>Dezeen displays list of job opportunities in chronological order (most recently posted)</li>
 					<li>Jos&eacute; scrolls through jobs and selects most fitting listing (by clicking on job title link)</li>
 					<li>Dezeen displays the selected full job description which contains the direct email of hiring personnel</li>
-					<li>Jos&eacute; submits all applicant requirements to said employer email</li>
+					<li>Jos&eacute; submits all applicant requirements to said employer email (cover letter, portfolio, website, etc.)</li>
 					<li>Employer receives Jose's submission, is "wow'd" by his accomplishments and sets up an interview</li>
 					<li>Jos&eacute; is pleased and drinks champagne in anticipation of getting a great new job! Lives happily ever after...</li>
 				</ol>
