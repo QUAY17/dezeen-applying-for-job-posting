edf191c728d5262608b33fc3b28fbae905b15507
Jennifer Minnich <jminniecc@gmail.com>
2017-01-11 21:42:25 -0700

    committing jose' main goal

--- index.php
+++ index.php
@@ -11,18 +11,23 @@
 			<h1 align="center">Applying for a job posting on Dezeen</h1>
 			</header>
 		<main>
-				<h2 align="left">Persona: Jose'</h2>
 			<img src="images/jose.jpg" alt="jose' ferrufino" align="left" width="50%">
-			<p>
-				<b>Name</b>: Jose' Ferrufino<br>
-				<b>Age</b>: 33<br>
-				<b>Profession</b>: Luxury Product Design Professional<br>
-				<b>Technology</b>: MacPro Desktop(@work- design studio) MacBook Pro(@home- newer model and updated IOS, external hard drive), iPhone6s or higher(@mobile- International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work). Absolutely efficient in Windows, but in the design industry, Mac is preferred.<br>
-				<b>Attitudes and Behaviors</b>:Seasoned young professional with over a decade of experience in luxury product design. Not applying for jobs below his salary or expectation scope. Works internationally; has been employed in New York, Paris, and Lasaunne, Switzerland. Elevated sensibility.<br>
-				<b>Frustrations and Needs</b>:Jose' is not looking to be hired in any other market aside from the luxury niche. He is not looking to work for a start- up either (except his own, "Les Chardons-Bleus" which is located in Paris- working with his team on his own time). Salary expectations must be met as well as product expectations. Seeking "long-term" with growth potential, new contracts, expansion, etc.<br>
-				<b>Goals</b>: Landing a full-time, high-end design position which will be "home" for at least 3 years. Currently residing in nyc, relocation is a welcomed option and will be considered given the appropriate remuneration.
-</p>
+			<h2 align="left">Persona: Jose'</h2>
+				<p><b>Name</b>: Jose' Ferrufino<br></p>
+				<p><b>Age</b>: 33</p>
+				<br>
+				<p><b>Profession</b>: Luxury Product Design Professional</p>
+				<br>
+				<p><b>Technology</b>: MacPro Desktop(@work- design studio) MacBook Pro(@home- newer model and updated IOS, external hard drive), iPhone6s or higher(@mobile- International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work). Absolutely efficient in Windows, but in the design industry, Mac is preferred.</p>
+				<br>
+				<p><b>Attitudes and Behaviors</b>:Seasoned young professional with over a decade of experience in luxury product design. Not applying for jobs below his salary or expectation scope. Works internationally; has been employed in New York, Paris, and Lasaunne, Switzerland. Elevated sensibility.</p>
+				<br>
+				<p><b>Frustrations and Needs</b>:Jose' is not looking to be hired in any other market aside from the luxury niche. He is not looking to work for a startup either (except his own, "Les Chardons-Bleus" which is located in Paris- working with his team on his own time). Salary expectations must be met as well as product expectations. Seeking "long-term" with growth potential, new contracts, expansion, etc.</p>
+				<br>
 				<h3>Use Case: Main Goal</h3>
+				<p>Landing a full-time, high-end design position in the US, Europe, or Asia which will be "home" for at least 3 years. Currently residing in nyc, relocation is a welcomed option and will be considered given the appropriate remuneration. As previously stated, emphasis on vertical growth potential within the company.</p>
+			<br>
+
 				<h3>Interaction Flow: Specific Steps</h3>
 		</main>
 		</body>
