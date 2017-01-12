5a572d2ac1a5d4922fcec9d63b06c4cd20bac1a7
Jennifer Minnich <jminniecc@gmail.com>
2017-01-11 19:12:52 -0700

    committing layout

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-Frontend:
-Define one (1) Persona for your assigned site.
-Document one (1) Use Case and one (1) Interaction Flow based on your Persona. Be detailed!
-What is the user's main goal?
-WHY, when, where, and with what technology will the Persona be using the site?
-What are the specific steps the user must take to arrive at that goal?
-
 <!DOCTYPE html>
 	<html lang="en">
 		<head>
@@ -18,14 +11,11 @@ What are the specific steps the user must take to arrive at that goal?
 			<h1>Applying for a job posting</h1>
 			</header>
 		<main>
-			<p>
-				Frontend:
-				Define one (1) Persona for your assigned site.
-				Document one (1) Use Case and one (1) Interaction Flow based on your Persona. Be detailed!
-				What is the user's main goal?
-				WHY, when, where, and with what technology will the Persona be using the site?
-				What are the specific steps the user must take to arrive at that goal?
-			</p>
+			<ul>
+				<li>Persona: Who</li>
+				<li>Use Case: Main Goal</li>
+				<li>Interaction Flow: Specific Steps</li>
+			</ul>
 		</main>
 		</body>
 </html>
