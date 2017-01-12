4eec0e856ecd5e126d09f794de65258c78e291e3
Jennifer Minnich <jminniecc@gmail.com>
2017-01-11 20:42:44 -0700

    committing pic of jose'

--- index.php
+++ index.php
@@ -8,14 +8,22 @@
 	</head>
 	<body>
 		<header>
-			<h1>Applying for a job posting</h1>
+			<h1 align="center">Applying for a job posting</h1>
 			</header>
 		<main>
-			<ul>
-				<li>Persona: Who</li>
-				<li>Use Case: Main Goal</li>
-				<li>Interaction Flow: Specific Steps</li>
-			</ul>
+				<h2 >Persona: Who</h2>
+			<img src="/images/jose.jpg"
+			<p>
+Name: Jose' Ferrufino
+Age: 33
+Profession: Luxury Product Design Professional
+Technology: MacPro Desktop(@work- design studio) MacBook Pro(@home- newer model and updated IOS, external hard drive), iPhone6s or higher(@mobile- International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work). Absolutely efficient in Windows, but in the design industry, Mac is preferred.
+Attitudes and Behaviors:
+Frustrations and Needs:
+Goals:
+</p>
+				<h3>Use Case: Main Goal</h3>
+				<h3>Interaction Flow: Specific Steps</h3>
 		</main>
 		</body>
 </html>
