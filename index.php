70ea31b453ad24f3364915044b40b3eb7afef61d
Jennifer Minnich <jminniecc@gmail.com>
2017-01-12 08:52:05 -0700

    committing grammar updates and image height

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 			<h1 align="center">Applying for a job posting on Dezeen</h1>
 			</header>
 		<main>
-			<img src="images/jose.jpg" alt="jose' ferrufino" align="left" width="50%">
+			<img src="images/jose.jpg" alt="jose' ferrufino" align="left" width="50%" height="50%">
 
 			<h2 align="left">Persona: Jos&eacute;</h2>
 				<p><b>Name</b>: Jos&eacute; Ferrufino<br></p>
@@ -17,10 +17,10 @@
 				<p><b>Profession</b>: Luxury Product Design Professional</p>
 			<p><b>Technology</b>:
 				<ul>
-				<li>MacPro Desktop(<b>work</b>: design studio)</li>
-				<li>MacBook Pro(<b>home</b>: newer model and updated IOS, external hard drive)</li>
-				<li>iPhone6s or higher(<b>mobile</b>: International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work)</li>
-				<li>Absolutely efficient in Windows, but in the design industry, Mac is preferred.</li>
+				<li><b>MacPro Desktop</b> (work: design studio)</li>
+				<li><b>MacBook Pro</b> (home: newer model and updated IOS, external hard drive)</li>
+				<li><b>iPhone6s or higher</b> (mobile: International data plan as Jose' travels a handful of times a year for work, was born in Montreal and has lived internationally for both school and work)</li>
+				<li>Proficient in Windows, but in the design industry, Mac is preferred.</li>
 			</ul>
 				<p><b>Attitudes and Behaviors</b>:Seasoned young professional with over a decade of experience in luxury product design. Not applying for jobs below his salary or expectation scope. Works internationally; has been employed in New York, Paris, and Lasaunne, Switzerland. Elevated sensibility.</p>
 				<p><b>Frustrations and Needs</b>:Jose' is not looking to be hired in any other market aside from the luxury niche. He is not looking to work for a startup either (except his own, "Les Chardons-Bleus" which is located in Paris- working with his team on his own time). Salary expectations must be met as well as product expectations. Seeking "long-term" with growth potential, new contracts, expansion, etc.</p>
@@ -33,9 +33,9 @@
 		<h3 align="left">Interaction Flow: Specific Steps</h3>
 				<ol>
 					<li>Jos&eacute; visits <a href="https://www.dezeenjobs.com">Dezeen Jobs</a></li>
-					<li>Dezeen displays list of job opportunities in most recent chronological order</li>
+					<li>Dezeen displays list of job opportunities in chronological order (most recently posted)</li>
 					<li>Jos&eacute; scrolls through jobs and selects most fitting listing (by clicking on job title link)</li>
-					<li>Dezeen displays full job description containing email of employer receiving applicants</li>
+					<li>Dezeen displays the selected full job description which contains the direct email of hiring personnel</li>
 					<li>Jos&eacute; submits all applicant requirements to said employer email</li>
 					<li>Employer receives Jose's submission, is "wow'd" by his accomplishments and sets up an interview</li>
 					<li>Jos&eacute; is pleased and drinks champagne in anticipation of getting a great new job! Lives happily ever after...</li>
