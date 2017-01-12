729a06b6a9a7531fa50f7687a123ee7c59971fd6
Jennifer Minnich <jminniecc@gmail.com>
2017-01-11 18:38:17 -0700

    committing bare bones html

---
+++ index.php
@@ -0,0 +1,31 @@
+Frontend:
+Define one (1) Persona for your assigned site.
+Document one (1) Use Case and one (1) Interaction Flow based on your Persona. Be detailed!
+What is the user's main goal?
+WHY, when, where, and with what technology will the Persona be using the site?
+What are the specific steps the user must take to arrive at that goal?
+
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8"/>
+<html>
+	<head>
+		<title>Dezeen</title>
+	</head>
+	<body>
+		<header>
+			<h1>Applying for a job posting</h1>
+			</header>
+		<main>
+			<p>
+				Frontend:
+				Define one (1) Persona for your assigned site.
+				Document one (1) Use Case and one (1) Interaction Flow based on your Persona. Be detailed!
+				What is the user's main goal?
+				WHY, when, where, and with what technology will the Persona be using the site?
+				What are the specific steps the user must take to arrive at that goal?
+			</p>
+		</main>
+		</body>
+</html>
